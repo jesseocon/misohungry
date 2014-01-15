92a8a2e76fce25184829af109bd871bb5a2e799a
Jesse Ocon <jesseocon@gmail.com>
2014-01-14 19:49:54 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Misohungry::Application.config.secret_token = '82ef2e6b175ef68bb395638f4c863c0b94a138a0290d86e9db52d289bf9c6cd9c4a87f42655f09732758a405128768934710664490971c4331d9f1c51908723f'
